84ad13b92da764d2bb6d3b82f3150f852f5f8679
wuchaoml <wuchaoml@gmail.com>
2018-04-01 15:29:34 +0800

    -

--- RL/Policy_gradient_softmax/run_MountainCar.py
+++ RL/Policy_gradient_softmax/run_MountainCar.py
@@ -1,6 +1,8 @@
 import gym
 from RL_brain import PolicyGradient
 import matplotlib.pyplot as plt
+import time
+
 
 DISPLAY_REWARD_THRESHOLD = -2000
 RENDER = False
@@ -20,16 +22,21 @@ RL = PolicyGradient(n_actions=env.action_space.n,
                     reward_decay=0.995,
                     )
 
+time_list = []
 for i_episode in range(1000):
     observation = env.reset()
+    start_time = time.time()
     while True:
         if RENDER:
             env.render()
-        action = RL.chooser_action(observation)
+        action = RL.choose_action(observation)
         observation_, reward, done, info = env.step(action)
         RL.store_transition(observation, action, reward)
 
         if done:
+            during_time = time.time() - start_time
+            time_list.append(during_time)
+
             ep_rs_sum = sum(RL.ep_rs)
             if 'running_reward' not in globals():
                 running_reward = ep_rs_sum
@@ -37,7 +44,8 @@ for i_episode in range(1000):
                 running_reward = running_reward * 0.99 + ep_rs_sum * 0.01
             if running_reward > DISPLAY_REWARD_THRESHOLD:
                 RENDER = True
-            print('episode:', i_episode, 'reward:', int(running_reward))
+            print('episode:', i_episode, 'reward:', int(
+                running_reward), 'time:', during_time)
             vt = RL.learn()
 
             if i_episode == 30:
@@ -47,3 +55,5 @@ for i_episode in range(1000):
                 plt.show()
             break
         observation = observation_
+
+print(time_list)
